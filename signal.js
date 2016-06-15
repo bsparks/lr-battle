654a6bf9fd262de52a054da3d5a99aeeadf6cbf3
Ben Sparks <bsparks42@gmail.com>
2016-06-15 00:20:45 -0700

    fix issue in signal where listeners added during dispatch would fire immediately

--- signal.js
+++ signal.js
@@ -4,8 +4,9 @@ class Signal {
     }
 
     dispatch(...params) {
-        for (let listener of this.listeners) {
-            listener.listener.call(listener.scope, ...params);
+        // iterate over array copy in case add/remove during dispatch
+        for (let listener of this.listeners.toArray()) {
+            listener.listener.call(listener.context, ...params);
             if (listener.ttl > 0) {
                 listener.ttl--;
             }
@@ -15,16 +16,16 @@ class Signal {
         }
     }
 
-    add(listener, scope = null, ttl = -1) {
+    add(listener, context = null, ttl = -1) {
         if(typeof listener !== 'function') {
             throw new TypeError('listener must be a function');
         }
-        let handler = {scope, ttl, listener};
+        let handler = {context, ttl, listener};
         this.listeners.add(handler);
     }
 
-    addOnce(listener, scope = null) {
-        this.add(listener, scope, 1);
+    addOnce(listener, context = null) {
+        this.add(listener, context, 1);
     }
 
     remove(listener) {
